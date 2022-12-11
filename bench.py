99aed69cdf45d8747827c870001a012eee4b73e8
gwpoole96@gmail.com <gwpoole96@gmail.com>
2022-12-11 16:56:46 -0600

    updated code

--- bench.py
+++ bench.py
@@ -1,30 +1,7 @@
 import os
-
-command = "bash get_tags.sh python"
-tag_file = "tags.txt"
-os.system(command)
-with open(tag_file) as f:
-    tag_list = f.read().split()
-os.remove(tag_file)
-print(tag_list)
-tag_list = filter(
-    lambda x: x[-1].isdigit(), tag_list
-)  # not a version based on a distro
-tag_list = filter(lambda x: x[0] == "3", tag_list)  # python 3
-tag_list = filter(
-    lambda x: not any(y.isalpha() for y in x), tag_list
-)  # filter out things that have letters
-tag_list = filter(
-    lambda x: len(x.split(".")) == 3, tag_list
-)  # want just versions with a patch version
-tag_list = filter(
-    lambda x: int(x.split(".")[1]) > 6, tag_list
-)  # 3.6 and below doesnt work with benchmark
-for version in tag_list:
-    os.system("podman system prune -a -f")
-    os.system(
-        f"podman build -f dockerfile -t clean/test:latest --build-arg tag={version}"
-    )
-    os.system("podman run --name tester clean/test:latest")
-    os.system(f"export container=$(podman ps -alq) && podman cp $container:/test/{version}.json .")
-    os.system(f"mkdir --parents ./results && mv ./{version}.json results/{version}.json")
+os_ver = os.environ['TOX_ENV_NAME']
+os.system(f"pyperformance run -f -o {os_ver}.json -b 2to3")
+newpath = 'results' 
+if not os.path.exists(newpath):
+    os.makedirs(newpath)
+os.rename(f"{os_ver}.json", f"results/{os_ver}.json")
--- dockerfile
+++ dockerfile
@@ -3,6 +3,6 @@ FROM python:${tag}
 ARG tag
 RUN mkdir test
 WORKDIR /test
-RUN python3 -m pip install --upgrade wheel
+RUN python3 -m pip install --upgrade pip wheel
 RUN python3 -m pip install pyperformance
 RUN pyperformance run -f -o ${tag}.json -b apps,serialize
