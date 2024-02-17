53f193bf24011081bdc17cad752c45a302e862e3
gwpoole96@gmail.com <gwpoole96@gmail.com>
2024-02-17 11:52:30 -0600

    updates

--- bench.py
+++ bench.py
@@ -1,8 +1,14 @@
 import sys
 import subprocess
+import argparse
 
-# Get the Python version
-os_ver = '.'.join(map(str, sys.version_info[:3]))
+# Create an argument parser
+parser = argparse.ArgumentParser(description='Run pyperformance with specified OS type and Python version.')
+parser.add_argument('--os_type', type=str, required=True, help='The OS type.')
+parser.add_argument('--python_ver', type=str, required=True, help='The Python version.')
+
+# Parse the arguments
+args = parser.parse_args()
 
 # Run pyperformance with specified benchmarks and output to a JSON file
-subprocess.run(["pyperformance", "run", "-f", "-o", f"{os_ver}.json", "-b", "2to3,chameleon,docutils,html5lib,tornado_http"], check=True)
+subprocess.run(["pyperformance", "run", "-f", "-o", f"{args.os_type}_{args.python_ver}.json", "-b", "2to3,chameleon,docutils,html5lib,tornado_http"], check=True)
